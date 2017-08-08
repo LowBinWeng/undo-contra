6e9bbe477b9b0a5d1075ef41d0d3c713dab0e12d
Shawn <shawnbecktp@gmail.com>
2017-08-08 18:00:14 +0800

    Simple Crosshair

---
+++ Assets/_UNDO/Prefabs/Player/CrossHair.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrossHair : MonoBehaviour {
+
+	public Transform target;
+	public Transform spriteRoot;
+
+	// Update is called once per frame
+	void LateUpdate () {
+		spriteRoot.LookAt (target);
+	}
+}
--- Assets/_UNDO/Scripts/GamePlay/Player/PlayerController.cs
+++ Assets/_UNDO/Scripts/GamePlay/Player/PlayerController.cs
@@ -4,14 +4,30 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+	[Header("Character")]
 	public float moveSpeed = 1f;
 	public Animator anim;
 
+	[Header("CrossHair")]
+	public Transform crossHair;
+	public Vector2 minBounds;
+	public Vector2 maxBounds;
+	public float aimSpeed = 1f;
+	Vector3 aimVelocity = Vector3.zero;
+	Vector3 lastMousePos = Vector3.zero;
+	Vector3 aimDelta = Vector3.zero;
+
 	Vector3 velocity = Vector3.zero;
+
+	void Awake() {
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+	}
 
 	// Update is called once per frame
 	void Update () {
 		ControlMovement ();
+		ControlCrossHair ();
 	}
 
 	void ControlMovement() {
@@ -23,6 +39,21 @@ public class PlayerController : MonoBehaviour {
 		anim.SetFloat ("Direction", velocity.x);
 		anim.SetFloat ("Speed", Mathf.Abs(velocity.x));
 
+	}
+
+	void ControlCrossHair() {
+		aimDelta = Input.mousePosition - lastMousePos;
+		aimDelta.Normalize ();
+
+		aimVelocity.x = aimDelta.x;
+		aimVelocity.y = aimDelta.y;
+
+		aimVelocity.x = Input.GetAxis ("Mouse X");
+		aimVelocity.y = Input.GetAxis ("Mouse Y");
+
+		crossHair.Translate (aimVelocity * Time.deltaTime * aimSpeed);
+
+		lastMousePos = Input.mousePosition;
 	}
 
 }
