1c76f0629224f47e8ff37f283470d7637d1d1178
Shawn <shawnbecktp@gmail.com>
2017-08-08 17:24:44 +0800

    Camera Work and basic controls

---
+++ Assets/_UNDO/Scripts/GamePlay/Enemy/Enemy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	public float frequency = 1f;
+	public float distance = 1f;
+
+	Vector3 newPos = Vector3.zero;
+	
+	// Update is called once per frame
+	void Update () {
+		newPos = transform.position;
+		newPos.x = (Mathf.Sin (Time.time * frequency) * distance);
+		transform.position = newPos;
+	}
+}
---
+++ Assets/_UNDO/Scripts/GamePlay/Player/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveSpeed = 1f;
+	public Animator anim;
+
+	Vector3 velocity = Vector3.zero;
+
+	// Update is called once per frame
+	void Update () {
+		ControlMovement ();
+	}
+
+	void ControlMovement() {
+
+		velocity.x = Input.GetAxis ("Horizontal");
+
+		transform.Translate (velocity * moveSpeed * Time.deltaTime);
+
+		anim.SetFloat ("Direction", velocity.x);
+		anim.SetFloat ("Speed", Mathf.Abs(velocity.x));
+
+	}
+
+}
