853a384582a4b460aef7b03bf6a16ab05e00edc6
Shawn Beck <shawnbecktp@gmail.com>
2017-12-05 08:33:57 +0800

    Menu BGM

---
+++ Assets/TitleMusicPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleMusicPlayer : MonoBehaviour {
+
+	public string musicName = "Menu";
+
+	// Use this for initialization
+	void Start () {
+		Invoke( "PlayMusic",0.1f);
+	}
+
+	void PlayMusic() {
+		AudioManager.Instance.PlayBGM("event:/"+musicName);		
+	}
+}
--- Assets/_UNDO/Scripts/UI/StartButton.cs
+++ Assets/_UNDO/Scripts/UI/StartButton.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 public class StartButton : MonoBehaviour {
 
 	public void StartGame() {
+
 		Debug.Log("StartGame");
+		AudioManager.Instance.StopBGM(true);
 		Time.timeScale = 1f;
 		TransitionManager.Instance.TransitionOut( 2, "Game" ); 
+
 	}
 }
